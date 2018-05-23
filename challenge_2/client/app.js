a7860c8fb0911f41f71445c7bcca896ab9321fb3
Xue Chen <xueqingc@gmail.com>
2018-05-22 20:50:32 -0700

    added functionality for bare minimum

--- challenge_2/client/app.js
+++ challenge_2/client/app.js
@@ -1,2 +1,80 @@
 
+$(document).ready(function(){
+
+
+
+var exampledata = {
+  "firstName": "Joshie",
+  "lastName": "Wyattson",
+  "county": "San Mateo",
+  "city": "San Mateo",
+  "role": "Broker",
+  "sales": 1000000,
+  "children": [
+  {
+    "firstName": "Beth Jr.",
+    "lastName": "Johnson",
+    "county": "San Mateo",
+    "city": "Pacifica",
+    "role": "Manager",
+    "sales": 2900000,
+    "children": [
+      {
+        "firstName": "Smitty",
+        "lastName": "Won",
+        "county": "San Mateo",
+        "city": "Redwood City",
+        "role": "Sales Person",
+        "sales": 4800000,
+        "children": []
+      },
+      {
+        "firstName": "Allen",
+        "lastName": "Price",
+        "county": "San Mateo",
+        "city": "Burlingame",
+        "role": "Sales Person",
+        "sales": 2500000,
+        "children": []
+      }
+    ]
+  },
+  {
+    "firstName": "Beth",
+    "lastName": "Johnson",
+    "county": "San Francisco",
+    "city": "San Francisco",
+    "role": "Broker/Sales Person",
+    "sales": 7500000,
+    "children": []
+  }
+]
+};
+
+
+
+$('form').on('submit', function(event){
+  event.preventDefault();
+  var text = $('textarea').val();
+  console.log(text);
+  $.ajax({
+    method: 'POST',
+    url: 'http://127.0.0.1:8080/',
+    data: JSON.stringify(exampledata),
+    contentType: 'application/json',
+    success: function(result){
+      console.log('data posted', result);
+      result.forEach(row=>{
+        $('.result').append(row + '<br/>');
+      })
+    },
+    error: (response) => console.log('POST failed', response)
+  });
+ 
+
+      //$('.result').html(data)
+  
+  })
+
+});
 
--- challenge_2/server.js
+++ challenge_2/server.js
@@ -1,2 +1,45 @@
+var express = require('express');
+var bodyParser = require('body-parser');
+var app = express();
+app.use(bodyParser.json());
+app.use(express.static('client'));
+
+app.get('/', (req, res)=>{
+  res.sendFile('index.html');
+})
+
+app.post('/', function(req, res){
+  // console.log('==============');
+  // res.end('POST request to the homepage');
+  var data = jsonTOCSV(req.body);
+  console.log(data);
+  res.send(data);
+})
+
+
+app.listen(8080, () => console.log('Example app listening on port 8080!'))
+
+
+var jsonTOCSV = function(data) {
+  console.log(typeof data);
+  var keys = Object.keys(data).splice(0,5).join(',');
+  var convertedData = [[keys]];
+  var recurse = function(data){
+    var array = [];
+    for (var i=0; i<data.length; i++) {
+      var values = Object.values(data[i]);
+      debugger;
+      convertedData.push([values.slice(0, 6).join(',')]);
+      if (values[6]){
+        recurse(values[6]);
+      }
+    }
+  }
+  recurse(data.children);
+  return convertedData;
+}
+
+
+
 
 
